ecca91ed73e55a60a66ea7abee634ebd0670aa9e
heshanxiu <nanyamashuu@heshanxiudeMacBook-Pro.local>
2020-03-10 20:45:22 -0700

    test/train

--- read_h5py.py
+++ read_h5py.py
@@ -2,6 +2,6 @@ import h5py
 import numpy as np
 
 
-with h5py.File("/local/shanxiu/lxmert/snap/visualsrl/visualsrl_extract_1/lxmert_feature", "r") as f:
+with h5py.File("/local/shanxiu/ubert/lxmert/snap/visualsrl/visualsrl_extract_1/lxmert_feature", "r") as f:
     h5_features = f['features'][0,0]
     print(h5_features)
--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 set PAT = "/Users/nanyamashuu/PycharmProjects/lxmert/"
 
 scp /User
-scp /Users/nanyamashuu/PycharmProjects/lxmert/run/visualsrl_extract.bash heshanxiu@nlp7.cs.ucla.edu:/local/shanxiu/lxmert/run/
-scp /Users/nanyamashuu/PycharmProjects/lxmert/src/tasks/visualsrl*.py heshanxiu@nlp7.cs.ucla.edu:/local/shanxiu/lxmert/src/tasks/
+scp /Users/nanyamashuu/PycharmProjects/lxmert/run/visualsrl_extract.bash heshanxiu@nlp7.cs.ucla.edu:/local/shanxiu/ubert/lxmert/run/
+scp /Users/nanyamashuu/PycharmProjects/lxmert/src/tasks/visualsrl*.py heshanxiu@nlp7.cs.ucla.edu:/local/shanxiu/ubert/lxmert/src/tasks/
